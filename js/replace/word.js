09bb92e2e60044a83044fd40a438d64bb6f4eecc
maikel <maykelfdz@hotmail.com>
2024-08-26 19:28:03 +0200

    funtion word scripting

--- js/replace/word.js
+++ js/replace/word.js
@@ -1,52 +1,47 @@
-var words = ['DEVELOPER', 'BACKEND'],
-    wordWrapper = document.getElementById('word'),
-    wordWrapperContent = wordWrapper.innerHTML,
-    addingWord = false,
-    counter = 1;
+document.addEventListener("DOMContentLoaded", function() {
+  var words = ['DEVELOPER', 'BACKEND'],
+      wordWrapper = document.getElementById('word'),
+      wordWrapperContent = '',
+      addingWord = false,
+      counter = 0;
 
-// setTimeout(runEscribir,2500);    
+  // setTimeout(runEscribir,2500);    
 
-// function runEscribir(){
-// setInterval(escribir, 230);
-// };
-//
-window.setInterval(escribir, 230);
+  // function runEscribir(){
+  // setInterval(escribir, 230);
+  // };
+  //
+  var changeWord = setInterval(escribir, 230);
 
-function escribir(){ 
-  
-  // while(!wordWrapperContent === "DEVELOPER"){
-  //   escribir();
-  // }
-  
-  if(wordWrapperContent.length > 0 && !addingWord ) {
-    wordWrapper.innerHTML = wordWrapperContent.slice(0, -1);
-    wordWrapperContent = wordWrapper.innerHTML;
-  } else {
-    addingWord = true;
-  }
-  
-  if(wordWrapperContent === "DEVELOPER") {
-    detener();
-  }
+  function escribir(){ 
 
-  if( addingWord ){
-    if( wordWrapperContent.length < words[counter].length  ) {
-      wordWrapper.innerHTML = words[counter].slice(0, wordWrapperContent.length + 1);
-      wordWrapperContent = wordWrapper.innerHTML;
-    } else {
-      if( counter < words.length) {
-        counter ++
+     // while(!wordWrapperContent === "DEVELOPER"){
+     //   escribir();
+     // }
+     
+     if(wordWrapperContent.length > 0 && !addingWord ) {
+          wordWrapper.innerHTML = wordWrapperContent.slice(0, -1);
+          wordWrapperContent = wordWrapper.innerHTML;
+      } else {
+          addingWord = true;
       }
-      addingWord = false;
-    }
-  }
-  
-  if( counter == words.length) {
-    counter = 0;
-  }
 
-};
+      if( addingWord ){
+          if( wordWrapperContent.length < words[counter].length ) {
+              wordWrapper.innerHTML = words[counter].slice(0, wordWrapperContent.length + 1);
+              wordWrapperContent = wordWrapper.innerHTML;
+          } else {
+              addingWord = false;
+              counter++;
+          }
+      }
+      
+      if( counter == words.length) {
+          counter = 0;
+      }
+  };
 
-function detener(){
-clearInterval(changeWord, 0);
-};
+  function detener(){
+      clearInterval(changeWord);
+  }
+});
